e784a7deb60a45fd97ca5026cd729302f2bdb43a
AsCross <tarasdudyak@gmail.com>
2015-02-22 20:46:19 +0200

    фулскрін на 2х активітях =)

--- app/src/main/java/com/example/greenq/fino/MainActivity.java
+++ app/src/main/java/com/example/greenq/fino/MainActivity.java
@@ -8,13 +8,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
     ImageButton imgBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.main);
+
+
+
 
         imgBtn = (ImageButton) findViewById(R.id.imageButtonPlay);
         imgBtn.setOnClickListener(new View.OnClickListener()
