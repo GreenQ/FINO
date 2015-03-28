e16877e6e34e28ea15f6f79f8dcde4ee52dac588
GreenQ <tenny47@gmail.com>
2015-03-28 13:11:03 +0200

    Выгрузка слова и картинок по id

--- app/src/main/java/com/example/greenq/fino/MarketActivity.java
+++ app/src/main/java/com/example/greenq/fino/MarketActivity.java
@@ -2,9 +2,11 @@ package com.example.greenq.fino;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import com.android.vending.billing.IInAppBillingService;
+import com.anjlab.android.iab.v3.BillingProcessor;
 
 /**
  * Created by GreenQ on 26.03.2015.
@@ -15,6 +17,10 @@ public class MarketActivity extends Activity {
     private RelativeLayout btn5000;
     private RelativeLayout btn10000;
     private RelativeLayout btn25000;
+    private int currentPrise;
+    private String currentPriseTag ="";
+    private boolean readyToPurchase;
+    private BillingProcessor bps;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
@@ -27,6 +33,36 @@ public class MarketActivity extends Activity {
         btn10000 = ( RelativeLayout ) findViewById( R.id.m4 );
         btn25000 = ( RelativeLayout ) findViewById( R.id.m5 );
 
+        if( btn25000 != null )
+        {
+            btn25000.setOnClickListener( new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clickPrised(25000, "4P1WRc25000" );
+                }
+            });
+        }
 
+
+    }
+
+    private void clickPrised(int integer , String id )
+    {
+
+        currentPrise = integer;
+        currentPriseTag = id;
+        if (!readyToPurchase) {
+            //showToast("Billing not initialized.");
+            return;
+        }
+        bps.purchase( currentPriseTag );
+//        bps.purchase("android.test.purchased");
+
+//        bps.getPurchaseTransactionDetails("coins_100");
+//        bps.release();
+//        Application.getInstance().getUserAccount().setUserPoint( integer );
+//        Intent intent = new Intent();
+//        setResult( Activity.RESULT_OK, intent );
+//        finish();
     }
 }
